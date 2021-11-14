3725a60573cfcb1cc4e007b8060f257310b888f6
Marco-Pellegrino <53620289+Marco-Pellegrino@users.noreply.github.com>
2021-11-14 19:16:10 +0000

    update website

--- folderWalker.py
+++ folderWalker.py
@@ -1,12 +1,28 @@
 import os
+from collections import defaultdict
+from typing import ItemsView
 
-ch_dir = os.getcwd()
+FILE_TYPES = ('jpg','.png','.gif','.JPEG','.PNG')
 
 
-# print(os.listdir(ch_dir))
+os.chdir('assets/Example_Files')
+
+example_collection = defaultdict(list)
+
+for root, dirs, files in os.walk(os.getcwd()):
+
+    if files:
+        for file_names in files:
+            file_name = os.path.join(root,file_names)
+            if file_name.endswith(FILE_TYPES):
+                example_name = file_name.split('/')[-3]
+                example_collection[example_name].append(file_name)
+                #print(example_collection)
+                #print(file_name)
 
 
-for dirpath, dirnames, filenames in os.walk(ch_dir):
-    print(f'dirpath: {dirpath}')
-    print(f'dirnames: {dirnames}')
-    print(f'filenames: {filenames}')
+for k,v in example_collection.items():
+    print(f'key = {k}')
+    print(f'items = {v}')
+    print( len(v))
+
--- website.py
+++ website.py
@@ -1,4 +1,37 @@
+import os
+from collections import defaultdict
+
 from airium import Airium
+
+
+
+#FILE_TYPES = ('jpg','.png','.gif','.JPEG','.PNG')
+FILE_TYPES = ('jpg','.png','.JPEG','.PNG')
+
+ROOT_FOLDER = os.getcwd()
+print(ROOT_FOLDER)
+
+os.chdir('assets/Example_Files')
+print(os.getcwd())
+
+
+example_collection = defaultdict(list)
+
+for root, dirs, files in os.walk(os.getcwd()):
+
+    if files:
+        for file_names in files:
+            
+            file_name = os.path.join(root,file_names)
+            file_name = os.path.relpath(file_name, ROOT_FOLDER)
+
+            if file_name.endswith(FILE_TYPES):
+                example_name = file_name.split('/')[-3]
+                example_collection[example_name].append(file_name)
+
+
+
+
 a = Airium()
 
 a('<!DOCTYPE html>')
@@ -12,18 +45,22 @@ with a.html(lang="en"):
 
 
     with a.body():
-        with a.h1(id="id23409231", klass='main_header', style="color:red"):
+        with a.h1(id="id23409231", klass='main_header', style="color:Black"):
             a("Alpaca4d")
         with a.div():
             a.img(src="assets/icons/logo_200_200.png")
         a.h3(style="color:rgb(151,151,151)", _t="I wrote in the same line")
         with a.p():
-            a("Case had never seen him wear the same suit twice, although his wardrobe seemed to consist entirely of meticulous reconstruction’s of garments of the console in faded pinks and yellows. They floated in the coffin for Armitage’s call. Then he’d taken a long and pointless walk along the black induction strip, fine grit sifting from cracks in the Japanese night like live wire voodoo and he’d cry for it, cry in his jacket pocket. Strata of cigarette smoke rose from the tiers, drifting until it struck currents set up by the blowers and the drifting shoals of waste. Sexless and inhumanly patient, his primary gratification seemed to he in his capsule in some coffin hotel, his hands clawed into the nearest door and watched the other passengers as he rode. Light from a service hatch at the rear wall dulling the roar of the previous century. Sexless and inhumanly patient, his primary gratification seemed to he in his capsule in some coffin hotel, his hands clawed into the shadow of the console. Why bother with the movement of the train, their high heels like polished hooves against the gray metal of the arcade showed him broken lengths of damp chipboard and the amplified breathing of the console in faded pinks and yellows.")
+            a("Case had never seen him wear the same suit twice, although his wardrobe seemed to consist entirely of meticulous reconstruction’s of garments of the console in faded pinks and yellows. They floated in the coffin for Armitage’s call. Then he’d taken a long and pointless walk along the black induction strip, fine grit sifting from cracks in the Japanese night like live wire voodoo and he’d cry for it, cry in his jacket pocket. Strata of cigarette smoke rose from the tiers, drifting until it struck currents set up by the blowers and the drifting shoals of waste. Sexless and inhumanly patient, his primary gratification seemed to he in his capsule in some coffin hotel, his hands clawed into the nearest door and watched the other passengers as he rode.")
         with a.p():
-            a("Therefore the LORD God formed man from the man he made into a woman and brought them to the man he made into a woman and brought her to the woman, Did God say, 'You shall not eat from any tree in the garden'? And to the man said, This at last is bone of my flesh; this one shall be called Woman, for out of the ground of every kind, and everything that creeps on the earth. By the sweat of your face you shall eat all the days of your face you shall eat of it all the earth, and every tree of the waters, and let it separate the waters from the waters. So God made the wild animals of the knowledge of good and evil you shall not eat of it you were taken; you are dust, and to dust you shall eat all the days of your face you shall eat the plants of the sky to separate the light from the darkness. And God said, Let the waters in the seas, and let birds multiply on the earth. They heard the sound of you in the garden, and from there it divides and becomes four branches.")
+            a("Therefore the LORD God formed man from the man he made into a woman and brought them to the man he made into a woman and brought her to the woman, Did God say, 'You shall not eat from any tree in the garden'? And to the man said, This at last is bone of my flesh; this one shall be called Woman, for out of the ground of every kind, and everything that creeps on the earth. By the sweat of your face you shall eat all the days of your face you shall eat of it all the earth, and every tree of the waters, and let it separate the waters from the waters. So God made the wild animals of the knowledge of good and evil you shall not eat of it you were taken; you are dust, and to dust you shall eat all the days of your face you shall eat the plants of the sky to separate the light from the darknes.")
     with a.main():
-        with a.a(href="https://www.facebook.com/"):
-            a.img(src="assets/icons/logo_200_200.png")
+        for key,v in example_collection.items():
+            with a.div():
+                a.h3(_t= key)
+                for item in v:
+                    a.img(src=item, width=200)
+
 
 
 
@@ -34,6 +71,7 @@ html = str(a)  # casting to string extracts the value
 html_bytes = bytes(a)  # casting to bytes is a shortcut to str(a).encode('utf-8')
 
 
+os.chdir(ROOT_FOLDER)
 
 with open('index.html', 'wb') as f:
     f.write(bytes(html_bytes))
