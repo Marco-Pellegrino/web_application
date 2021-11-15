dc0da9a1d790a9881ec6d2493dade50f00df0ac3
Marco-Pellegrino <53620289+Marco-Pellegrino@users.noreply.github.com>
2021-11-15 22:58:44 +0000

    only 3 images for folder

--- website.py
+++ website.py
@@ -13,16 +13,20 @@ def rec_split(s):
 
 
 IMAGE_TYPES = ('jpg','.png','.JPEG','.JPG','.PNG')
+GRASSHOPPER_TYPE = ('.gh')
 TEXT_TYPES = ('txt','.md')
 COLORS = ['#F5F5DC','#A52A2A','#8A2BE2','#0000FF','#008B8B']
 ROOT_FOLDER = os.getcwd()
-print(ROOT_FOLDER)
+
+#/Users/mpmac/Documents/GitHub/web_application
+print(ROOT_FOLDER) 
 
 os.chdir('assets/Example_Files')
 print(os.getcwd())
 
 
 example_image_collection = defaultdict(list)
+example_grasshopper = defaultdict(str)
 
 
 for root, dirs, files in os.walk(os.getcwd()):
@@ -34,11 +38,13 @@ for root, dirs, files in os.walk(os.getcwd()):
             file_name = os.path.relpath(file_name, ROOT_FOLDER)
 
             if file_name.endswith(IMAGE_TYPES):
-                print(file_name)
-                #example_name = file_name.split('\\')[-3]
                 example_name = rec_split(file_name)[-3]
                 example_image_collection[example_name].append(file_name)
-
+            
+            if file_name.endswith(GRASSHOPPER_TYPE):
+                example_name = rec_split(file_name)[-2]
+                example_grasshopper[example_name] = file_name
+            
 
 
 a = Airium()
@@ -52,13 +58,16 @@ with a.html(lang="en"):
         a.meta(name="viewport", content= "width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0")
         a.link(rel="icon", href= "assets/icons/logo_200_200.png")
 
+        a.link(rel="stylesheet", href="./style.css")
+        a.link(href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap",rel="stylesheet")
+
 
     with a.body():
         with a.h1(id="id23409231", klass='main_header', style="color:Black"):
             a("Alpaca4d")
         with a.div():
             a.img(src="assets/icons/logo_200_200.png")
-        a.h3(style="color:rgb(151,151,151)", _t="I wrote in the same line")
+
         with a.p():
             a("Case had never seen him wear the same suit twice, although his wardrobe seemed to consist entirely of meticulous reconstruction’s of garments of the console in faded pinks and yellows. They floated in the coffin for Armitage’s call. Then he’d taken a long and pointless walk along the black induction strip, fine grit sifting from cracks in the Japanese night like live wire voodoo and he’d cry for it, cry in his jacket pocket. Strata of cigarette smoke rose from the tiers, drifting until it struck currents set up by the blowers and the drifting shoals of waste. Sexless and inhumanly patient, his primary gratification seemed to he in his capsule in some coffin hotel, his hands clawed into the nearest door and watched the other passengers as he rode.")
         with a.p():
@@ -66,14 +75,15 @@ with a.html(lang="en"):
     
     with a.main():
         for key,v in example_image_collection.items():
-            with a.div(style="background-color: {0}".format(COLORS[random.randint(0,4)])):
-                a.h3(style = "color:#ffffff",_t= key)
+            with a.div(klass='examples', style="background-color: #ffffff"):
+                a.h3(style = "color:#000000",_t= key)
                 with a.p():
                     for item in v:
                         a.img(src=item, width=300)
                 
-                
-                a.a(href="www.google.com", style= "color:black", _t= "DOWNLOAD")
+                a.p(_t="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Ne assum fierent his, cibo eruditi necessitatibus id vix, cum tantas audiam reprehendunt in.")
+
+                a.a(klass="download_link",href=example_grasshopper[key], _t= "DOWNLOAD")
                 
 
 
